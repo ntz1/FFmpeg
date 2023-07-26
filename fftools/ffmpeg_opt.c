6603e5b6a29cb529c3174d2faac21d2dd8458ba5
ngoitz <101093006+ngoitz@users.noreply.github.com>
2023-07-26 20:43:03 +0200

    Revert "fftools/ffmpeg_opt: Fix leak on error"
    This reverts commit 8fbba04c2c367a14a0215e8368b2cf767dfb91a3.

--- fftools/ffmpeg_opt.c
+++ fftools/ffmpeg_opt.c
@@ -401,14 +401,13 @@ static int opt_map(void *optctx, const char *opt, const char *arg)
 
         ret = GROW_ARRAY(o->stream_maps, o->nb_stream_maps);
         if (ret < 0)
-            goto fail;
+            return ret;
 
         m = &o->stream_maps[o->nb_stream_maps - 1];
         m->linklabel = av_get_token(&c, "]");
         if (!m->linklabel) {
             av_log(NULL, AV_LOG_ERROR, "Invalid output link label: %s.\n", map);
-            ret = AVERROR(EINVAL);
-            goto fail;
+            return AVERROR(EINVAL);
         }
     } else {
         if (allow_unused = strchr(map, '?'))
@@ -416,8 +415,7 @@ static int opt_map(void *optctx, const char *opt, const char *arg)
         file_idx = strtol(map, &p, 0);
         if (file_idx >= nb_input_files || file_idx < 0) {
             av_log(NULL, AV_LOG_FATAL, "Invalid input file index: %d.\n", file_idx);
-            ret = AVERROR(EINVAL);
-            goto fail;
+            return AVERROR(EINVAL);
         }
         if (negative)
             /* disable some already defined maps */
@@ -440,7 +438,7 @@ static int opt_map(void *optctx, const char *opt, const char *arg)
                 }
                 ret = GROW_ARRAY(o->stream_maps, o->nb_stream_maps);
                 if (ret < 0)
-                    goto fail;
+                    return ret;
 
                 m = &o->stream_maps[o->nb_stream_maps - 1];
 
@@ -455,19 +453,16 @@ static int opt_map(void *optctx, const char *opt, const char *arg)
         } else if (disabled) {
             av_log(NULL, AV_LOG_FATAL, "Stream map '%s' matches disabled streams.\n"
                                        "To ignore this, add a trailing '?' to the map.\n", arg);
-            ret = AVERROR(EINVAL);
-            goto fail;
+            return AVERROR(EINVAL);
         } else {
             av_log(NULL, AV_LOG_FATAL, "Stream map '%s' matches no streams.\n"
                                        "To ignore this, add a trailing '?' to the map.\n", arg);
-            ret = AVERROR(EINVAL);
-            goto fail;
+            return AVERROR(EINVAL);
         }
     }
-    ret = 0;
-fail:
+
     av_freep(&map);
-    return ret;
+    return 0;
 }
 
 static int opt_attach(void *optctx, const char *opt, const char *arg)
